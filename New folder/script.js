6189e94da88395256b942397fed8dab232421c06
Gyan Vardhan <vardhan8343@gmail.com>
2026-09-10 16:18:53 +0530

    Fix backend API URL

--- New folder/script.js
+++ New folder/script.js
@@ -52,7 +52,7 @@ async function generateSummary() {
 
 
         const response = await fetch(
-            "http://127.0.0.1:5000/solve",
+            "https://smartstudy-ai-1-fj1s.onrender.com/solve",
             {
                 method: "POST",
                 body: formData
